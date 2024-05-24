565b88b62ee1f07ac35695a77d91b9bdee3d37ad
Kauan Victor <Kauan.victor@iesb.edu.br>
2024-05-23 21:51:20 -0300

    JWT para cifrar token

--- Aulas/Aula11/controllers/users.js
+++ Aulas/Aula11/controllers/users.js
@@ -1,18 +1,29 @@
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken')
 const Usuario = require('../models/users');
 
+function cifrarSenha(senha, salt) {
+    const hash = crypto.createHmac('sha256', salt);
+    hash.update(senha);
+    return hash.digest('hex');
+}
+
 async function criar(req, res) {
-    const novoUsuiario = await Usuario.create(req.body);
-    res.status(201).json({ id: novoUsuiario._id.toString(), email: novoUsuiario.email });
+    const salt = crypto.randomBytes(16).toString('hex');
+    const { email, senha } = req.body;
+
+    const novoUsuario = await Usuario.create({ email, senha: cifrarSenha(senha, salt), salt });
+    res.status(201).json({ id: novoUsuario._id.toString(), email: novoUsuario.email, senha: novoUsuario.senha, salt: novoUsuario.salt });
 }
 
 
 async function entrar(req, res) {
-    const usuario = await Usuario.findOne({email: req.body.email});
-    if (usuario.senha === req.body.senha){
-        res.json({token: '12345678'})
-    } else{
-        res.status(401).status({msg: 'acesso negado'});
-    } 
+    const usuario = await Usuario.findOne({ email: req.body.email });
+    if (usuario.senha === cifrarSenha(req.body.senha, usuario.salt)) {
+        res.json({ token: jwt.sign({ email: usuario.email }, '', { expiresIn: '1m' }) })
+    } else {
+        res.status(401).status({ msg: 'acesso negado' });
+    }
 }
 
 module.exports = { criar, entrar }
